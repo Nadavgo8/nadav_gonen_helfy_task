ad6632f69412022bfc3a5f95ec0aaec8d75434fd
Nadavgo8 <19nadav99@gmail.com>
2026-02-03 12:53:57 +0200

    added implementation to get tasks and add new task

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,11 +1,26 @@
 import { Router } from "express";
 
+let tasks = [];
+let numOfTasks = 0;
 const router = Router();
 router.get("/", (req, res) => {
     console.log("get tasks");
+    res.json(tasks);
 })
 router.post("/", (req, res) => {
     console.log("post tasks");
+    const { title, description, priority } = req.body;
+    const task = {
+        id: ++numOfTasks,
+        title,
+        description,
+        completed: false,
+        createdAt: new Date().toDateString(),
+        priority,
+  };
+
+  tasks.push(task);
+  res.status(201).json(task);
 })
 router.get("/:id", (req, res) => {
     console.log("get task with id");
