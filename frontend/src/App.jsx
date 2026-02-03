bf6a168d9e7f7940b6ab3660ff8bd3834e8bec1d
Nadavgo8 <19nadav99@gmail.com>
2026-02-03 14:25:42 +0200

    feat: initialize frontend with Vite and React setup
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in public directory.
    - Created main application structure in App.jsx with basic counter functionality.
    - Added React logo SVG in assets directory.
    - Created components for TaskList, TaskForm, TaskFilter, and TaskItem.
    - Implemented global styles in index.css and App.css.
    - Configured Vite in vite.config.js for React support.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useMemo, useState } from "react";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import TaskFilter from "./components/TaskFilter";
+import './styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
